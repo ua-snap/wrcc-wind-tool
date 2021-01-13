9d80d57927828b2e90b00f38a4b1dd397fbc41ed
kyleredilla <kmredilla@alaska.edu>
2021-01-13 01:07:08 -0900

    working script to process raw downloaded IEM ASOS data

--- process_raw.py
+++ process_raw.py
@@ -21,23 +21,44 @@ def read_and_filter_csv(fp):
     """
     dtype_dict = {key: np.float16 for key in ["drct", "sped", "gust_mph"]}
     df = pd.read_csv(fp, header=0, na_values="M", dtype=dtype_dict)
-    df.valid = pd.to_datetime(df.valid.values)
+    df["valid"] = pd.to_datetime(df["valid"].values)
+
+    # handle observations with erroneous combination of direction/speed
+    # The case where speed is 0 and direction is not is and invalid measurement.
+    # Not common (<600 for entire dataset), and there is no way to know whether the error was
+    # in speed or direction measurement. Drop them.
+    df = df.drop(
+        df[(df["sped"] == 0) & (~np.isnan(df["drct"])) & (df["drct"] != 0)].index
+    )
+
+    # As of now, not handling cases where speed is nonzero and direction is zero.
+    # There are 100 times as many of these as the first case. Instead of throwing these away,
+    # leave as is - it appears likely that these are observations
+    # of light and variable wind, as seen in METARs e.g. for PAEI on 2011-10-17
+    # df.loc[(df.ws != 0) & (~np.isnan(df.ws)) & (df.wd == 0), "drct"] = np.nan
+
+    # set negative wind gusts to NaN
+    df.loc[df["gust_mph"] < 0, "gust_mph"] = np.nan
 
     # Filter to hourly obs closest to "on the hour"
     # (this represents the most likely "routine" METAR record)
     # done by finding record with minimum timedelta from hour
-    df["valid_h"] = df.valid.dt.round("H")
-    df["delta_dt"] = abs((df.valid_h - df.valid))
+    df["ts"] = df["valid"].dt.round("H")
+    df["delta_dt"] = abs((df["ts"] - df["valid"]))
     min_dt_df = (
-        df.groupby(["valid_h"])
-        .agg(min_delta_dt=pd.NamedAgg("delta_dt", "min"))
-        .reset_index()
+        df.groupby("ts").agg(min_delta_dt=pd.NamedAgg("delta_dt", "min")).reset_index()
     )
 
     df = pd.merge(df, min_dt_df)
     df["keep"] = df["delta_dt"] == df["min_delta_dt"]
+    # filter to "keep" values and drop duplicates
+    # (duplicates note: in some cases, a SPECI observation was made the exact number of
+    # minutes after the hour that the routine METAR was made before the
+    # hour.)
+    df = df[df["keep"]].drop_duplicates(subset="ts", keep="first")
 
-    return df[df["keep"]].drop(columns=["valid_h", "delta_dt", "min_delta_dt"])
+    # don't need columns used to filter to hourly obs
+    return df.drop(columns=["valid", "delta_dt", "min_delta_dt", "keep"])
 
 
 def run_read_data(fps, ncpus):
@@ -49,6 +70,8 @@ def run_read_data(fps, ncpus):
     Returns:
         pandas.DataFrame of all individual sites
     
+    Notes:
+        The timestamp in the returned data is the NEAREST HOUR of the obs
     """
     print(f"Reading data using {ncpus} cores", end="...")
     tic = time.perf_counter()
@@ -58,7 +81,7 @@ def run_read_data(fps, ncpus):
 
     print(f"done, {round(time.perf_counter() - tic, 2)}s")
 
-    return df
+    return df.rename(columns={"station": "sid", "sped": "ws", "drct": "wd"})
 
 
 def main():
